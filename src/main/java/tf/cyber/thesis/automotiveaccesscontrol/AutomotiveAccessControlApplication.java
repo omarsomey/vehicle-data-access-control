a1194b94cb362cf3e1a7b35fb4bbf09e8c259d5e
Simon Wagner <simon@simon-wagner.eu>
2022-01-05 18:58:40 +0100

    general: initialize spring project

---
+++ src/main/java/tf/cyber/thesis/automotiveaccesscontrol/AutomotiveAccessControlApplication.java
@@ -0,0 +1,13 @@
+package tf.cyber.thesis.automotiveaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveAccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tf/cyber/thesis/automotiveaccesscontrol/AutomotiveAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package tf.cyber.thesis.automotiveaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
