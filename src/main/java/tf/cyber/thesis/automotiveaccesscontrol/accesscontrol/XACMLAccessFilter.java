72dc228c33a8228e8f667dd2ddb00b227f666ae4
Simon Wagner <simon@simon-wagner.eu>
2022-01-09 14:06:16 +0100

    clear unnecessary imports

--- src/main/java/tf/cyber/thesis/automotiveaccesscontrol/accesscontrol/XACMLAccessFilter.java
+++ src/main/java/tf/cyber/thesis/automotiveaccesscontrol/accesscontrol/XACMLAccessFilter.java
@@ -21,7 +21,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.Instant;
 import java.util.Objects;
 import java.util.Optional;
 
