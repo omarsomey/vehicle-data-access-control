e45484199bef3d7c7fe32f8a3dae6bee4ce1a233
Simon Wagner <simon@simon-wagner.eu>
2022-04-23 17:01:56 +0200

    resourcemanager: move port

--- smartsurance/src/config.js
+++ smartsurance/src/config.js
@@ -1,6 +1,6 @@
 export default {
     dataSource: 'http://192.168.248.123:8080/',
-    resourceManager: 'http://192.168.248.123:9999/',
+    resourceManager: 'http://192.168.248.123:7777/',
     deviceMaj: 179,
     deviceMin: 0,
     username: "smartsurance",
