a27bc00765da8d7a8d6923b31c32c63448187f39
Simon Wagner <simon@simon-wagner.eu>
2022-04-23 16:54:12 +0200

    more cors stuff

--- resourcemanager/src/main/java/tf/cyber/resourcemanager/config/SecurityConfiguration.java
+++ resourcemanager/src/main/java/tf/cyber/resourcemanager/config/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5555/", "http://192.168.248.62:5555/"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5555/", "http://192.168.248.62:5555/", "http://192.168.248.123:5555/"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
